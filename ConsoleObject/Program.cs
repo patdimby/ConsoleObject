6a349fb07e4eac20061d54f189a36bd7e73ab52c
patrickmadasoftware <patrickmadasoftware@gmail.com>
2021-04-13 14:13:05 +0200

    first test

--- ConsoleObject/Program.cs
+++ ConsoleObject/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace ConsoleObject
+﻿using System;
+using System.Security;
+
+namespace ConsoleObject
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            try
+            {
+                GrcEntite myObject = new GrcEntite();
+
+                // Get the type of 'GrcEntite'.
+                Type myType = myObject.GetType();
+
+                // Get the information related to all public member's of 'GrcEntite'.
+                var myMemberInfo = myType.GetMembers();
+
+                Console.WriteLine("\nThe members of class '{0}' are :\n", myType);
+                foreach (var t in myMemberInfo)
+                {
+                    // Display name and type of the concerned member.
+                    Console.WriteLine("'{0}' is a {1}", t.Name, t.MemberType);
+                }
+            }
+            catch (SecurityException e)
+            {
+                Console.WriteLine("Exception : " + e.Message);
+            }
         }
     }
 }
