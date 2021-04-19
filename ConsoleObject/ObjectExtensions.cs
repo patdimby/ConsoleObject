d12684731bf9cf0344f6c584e36a3eaabb92e206
patrickmadasoftware <patrickmadasoftware@gmail.com>
2021-04-19 12:35:29 +0200

    error add label view

--- ConsoleObject/ObjectExtensions.cs
+++ ConsoleObject/ObjectExtensions.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
 
 namespace ConsoleObject
 {
     public static class ObjectExtensions
-    {     
+    {
+        private static bool IsDefault<T>(this T val)
+        {
+            return EqualityComparer<T>.Default.Equals(val, default(T));
+        }
 
         public static void BuiltInCast(object dest, object src)
         {
@@ -24,30 +26,16 @@ namespace ConsoleObject
                 var primType = t.PropertyType.Name;
                 // Check if key exist in dictionary.
                 if (dict.ContainsKey(key))
-                {                    
+                {
                     // The value in dictionary.
                     var value = dict[key];
                     var vTypeName = value.GetType().Name;
-                    if(vTypeName == primType)
+                    if (vTypeName == primType)
                     {
                         t.SetValue(dest, value);
-                    }                    
+                    }
                 }
             }
-        }
-
-        private static bool IsNumeric(this object obj)
-        {
-            string s = obj.ToString();
-            if (!Regex.IsMatch(s, @"^\d+$")) return false;
-            var r = double.Parse(s);
-            return !(Math.Abs(r) < 0.0001);
-        }
-
-        private static bool IsValidString(this object obj)
-        {
-            string s = obj.ToString();
-            return s.Length > 0;
         }
 
         private static Dictionary<string, object> ObjectToDictionary(this object obj)
@@ -58,11 +46,10 @@ namespace ConsoleObject
             var result = new Dictionary<string, object>();
             foreach (var t in myType.GetProperties())
             {
-                var value = t.GetValue(obj, null);                
-                if (value != null)
+                var value = t.GetValue(obj, null);
+                if (!value.IsDefault())
                 {
-                    if (value.IsNumeric() || value.IsValidString())
-                    { result.Add(t.Name, value); }
+                    result.Add(t.Name, value);
                 }
             }
 
